ad2c6e0f518ffe0a1fe511ba2d1eccc2877791e4
Or Bellash <or.bellash@gmail.com>
2018-01-14 10:00:35 +0200

    Local dependency

--- scripts/App.js
+++ scripts/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import Scroll2TopButton from 'react-scroll2top-button';
+
 export default class App extends Component {
   render() {
     return (
-      // Add your component markup and other subcomponent references here.
+    // Add your component markup and other subcomponent references here.
       <div>
-        <h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1><h1>Hello, World!</h1>
-        <Scroll2TopButton/>
-      </div>
-    );
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <h1>Hello, World!</h1>
+        <Scroll2TopButton />
+      </div>);
   }
 }
